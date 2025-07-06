0b7be522a8410ea30c7aaf252f212813012285e9
Francisco Fernandez <fran6602@gmail.com>
2025-07-06 16:35:59 +0100

    chore: initialize dashboard template

---
+++ src/pages/dashboard/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="dashboard">
+      <h1>¡Bem-vindo ao Dashboard!</h1>
+    </div>
+</template>
+
+<style scoped>
+.dashboard {
+text-align: center;
+
+}
+</style>
+  
---
+++ src/router/routes.js
@@ -0,0 +1,8 @@
+import Dashboard from "@/pages/dashboard/index.vue";
+
+const routes = [
+  { path: "/", component: Dashboard, meta: { layout: "default" } },
+];
+  
+export default routes
+  
